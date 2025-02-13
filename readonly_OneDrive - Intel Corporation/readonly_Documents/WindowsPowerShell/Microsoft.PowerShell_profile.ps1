adcb0b96a570e4ff7cb131b418675c4635c204be
Shan Lee <shanlee41@gmail.com>
2025-02-13 11:15:19 +0800

    Add starship

---
+++ readonly_OneDrive - Intel Corporation/readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+#$Env:KOMOREBI_AHK_EXE = "C:\Program Files\AutoHotkey\v2\AutoHotkey.exe"
+$Env:KOMOREBI_AHK_EXE = "C:\Users\shanneng\AppData\Local\Programs\AutoHotkey\v2\AutoHotkey64.exe"
+Invoke-Expression (&starship init powershell)
