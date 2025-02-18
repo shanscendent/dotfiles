6bc12cd9dddb1ede1d0062469782bf709b5f6349
Shan Lee <shan.neng.lee@intel.com>
2025-02-18 09:45:15 +0800

    Gruvbox dark contrast

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,15 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      require("gruvbox").setup({
+        contrast = "hard",
+        dim_inactive = false,
+        transparent_mode = false,
+      })
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
