df1888b0ad65fdcd8b83e141d05d728cd3b3d4e1
Shan Lee <--local>
2025-05-25 08:40:19 +0800

    Add hidden property to snacks pickers

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -44,6 +44,27 @@ return {
         })
       end,
     },
+    -- Enable hidden for all the pickers, pretty sure there might be a better way of doing this?
+    -- TODO: find a better way to add options to Snacks_picker
+    { "<leader>/", LazyVim.pick("grep", { hidden = true }), desc = "Grep (Root Dir)" },
+    { "<leader><space>", LazyVim.pick("files", { hidden = true }), desc = "Find Files (Root Dir)" },
+    { "<leader>ff", LazyVim.pick("files", { hidden = true }), desc = "Find Files (Root Dir)" },
+    { "<leader>fF", LazyVim.pick("files", { hidden = true }, { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>fr", LazyVim.pick("oldfiles", { hidden = true }), desc = "Recent" },
+    { "<leader>sg", LazyVim.pick("live_grep", { hidden = true }), desc = "Grep (Root Dir)" },
+    { "<leader>sG", LazyVim.pick("live_grep", { hidden = true }, { root = false }), desc = "Grep (cwd)" },
+    {
+      "<leader>sw",
+      LazyVim.pick("grep_word", { hidden = true }),
+      desc = "Visual selection or word (Root Dir)",
+      mode = { "n", "x" },
+    },
+    {
+      "<leader>sW",
+      LazyVim.pick("grep_word", { root = false, hidden = true }),
+      desc = "Visual selection or word (cwd)",
+      mode = { "n", "x" },
+    },
   },
   init = function()
     vim.api.nvim_create_autocmd("User", {
