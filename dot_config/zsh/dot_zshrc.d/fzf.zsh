e1abed1eaa49fbbb64e48a1ee3606b720cf04fa4
Shan Lee <--local>
2025-05-25 23:50:32 +0800

    Move zsh config to ZDOTDIR

---
+++ dot_config/zsh/dot_zshrc.d/fzf.zsh
@@ -0,0 +1,8 @@
+FZF_ALT_C_COMMAND= source <(fzf --zsh)
+export FZF_CTRL_T_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+export FZF_TMUX_OPTS=" -p90%,70% "
+export FZF_DEFAULT_OPTS="--height 50% --layout=default --border --color=hl:#2dd4bf"
+
