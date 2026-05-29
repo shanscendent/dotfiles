552c276e21bc44b9c1adcf9e17189f22d3dfb785
Shan Lee <shanlee41@gmail.com>
2026-05-29 11:13:55 +0800

    Fix nvim file picker/finder behaviour

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- force always cwd for find files/picker, etc
+vim.g.root_spec = { "cwd" }
+
 vim.o.clipboard = "unnamedplus"
 
 local function paste()
