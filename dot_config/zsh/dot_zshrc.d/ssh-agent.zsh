95f3fae5e40f51551e6955aa3d9acce41fce7855
Shan Lee <shanlee41@gmail.com>
2026-06-05 12:22:42 +0800

    Add zsh stuff

--- dot_config/zsh/dot_zshrc.d/ssh-agent.zsh
+++ dot_config/zsh/dot_zshrc.d/ssh-agent.zsh
@@ -1,2 +1,6 @@
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
-
+# install keychain for ssh key management on Linux (optional)
+if command -v keychain >/dev/null 2>&1; then
+    eval "$(keychain --quiet --eval --agents ssh id_ed25519)"
+else
+    export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+fi
